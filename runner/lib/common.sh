30e2fae7290fb160f3e9dd15a79df4bf1162e0c6
dazuimao1990 <33855684+dazuimao1990@users.noreply.github.com>
2022-01-25 09:03:59 +0800

    multi-db instances support

--- runner/lib/common.sh
+++ runner/lib/common.sh
@@ -4,6 +4,17 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 RED='\033[1;31m'
 NC='\033[0m' # No Color
+
+function addPath() {
+    # run this function for add path to $PATH
+    # make binary which built in /app executable by default
+    local path=""
+    for bindir in $(find /app/ -type d -name *bin)
+    do 
+    path=$path:$bindir
+    done
+    echo "export PATH=$PATH$path" > ~/.bashrc
+}
 info() {
     echo -e "${GREEN}       $@${NC}"
 }
--- runner/lib/sqlvercontrol.sh
+++ runner/lib/sqlvercontrol.sh
@@ -6,45 +6,67 @@
 # 目前仅支持 Mysql 类型数据库的对接
 
 function detectSqlControl() {
-    CHANGE_LOG_FILE=${CHANGE_LOG_FILE:-"rainsql/changelog.sql"}
-    if [ -f $HOME/${CHANGE_LOG_FILE} ]; then
-        info "Database version control file ${CHANGE_LOG_FILE} has been found..."
-        dealWithConfig
-        runLiquiCmd || return 0
+    local schema_dir=$1
+    # skip schema version control without  schema dir
+    if [ ! -d $schema_dir ]; then
+        return 0
     fi
+    # cd schema dir
+    pushd $schema_dir >/dev/null
+    # find *.properties in schema dir
+    # deal with every properties file
+    for config_file in $(ls *.properties); do
+        info "=== Database version control file $config_file has been found ==="
+        dealWithConfig $config_file
+        runLiquiCmd $config_file || continue
+    done
+    popd >/dev/null
 }
 
 function dealWithConfig() {
     # Checks whether a configuration file exists named liquibase.properties
     # Render the template configuration file using environment variables
     # Currently, only mysql is supported
-    LIQUIBASE_CONFIG_FILE=${LIQUIBASE_CONFIG_FILE:-"rainsql/liquibase.properties"}
-    if ! grep "__MYSQL_HOST__" $HOME/${LIQUIBASE_CONFIG_FILE} >/dev/null; then
-        info "Useing custom liquibase config file ${LIQUIBASE_CONFIG_FILE}"
-    elif [ ! -z $MYSQL_USER ] && [ ! -z $MYSQL_PASSWORD ] && [ ! -z $MYSQL_DATABASE ]; then
-        info "Rendering ${LIQUIBASE_CONFIG_FILE} using environment variables"
-        sed -i -e "s/__MYSQL_HOST__/${MYSQL_HOST:-127.0.0.1}/g" \
-            -e "s/__MYSQL_PORT__/${MYSQL_PORT:-3306}/g" \
-            -e "s/__MYSQL_USER__/${MYSQL_USER:-root}/g" \
-            -e "s/__MYSQL_PASSWORD__/${MYSQL_PASSWORD:-""}/g" \
-            -e "s/__MYSQL_DATABASE__/${MYSQL_DATABASE}/g" \
-            -e "s/changelog.sql/${CHANGE_LOG_FILE#*/}/g" ${LIQUIBASE_CONFIG_FILE}
-    else
-        warn "Whether appropriate mysql connection environment variables have been defined?"
-        warn "Declare \$MYSQL_USER \$MYSQL_PASSWORD \$MYSQL_HOST \$MYSQL_PORT \$MYSQL_DATABASE please."
+    local config_file=$1
+    info "Customing liquibase config file $config_file"
+    # Just skip login liquibase hub cloud service
+    if ! (grep "liquibase.hub.mode*" $config_file >/dev/null); then
+        echo "liquibase.hub.mode=off" >>$config_file
     fi
+    envToConfig $config_file
 }
 
 function runLiquiCmd() {
+    local config_file=$1
     # Checks change sets before update
     # If there is no Changeset to be executed, do not update
-    LIQUIBASE_TEMP_FILE=${LIQUIBASE_TEMP_FILE:-"rainsql/cmd.log"}
+    local log_file=$config_file.log
     # run liquibase status, log to temp file
-    pushd ${CHANGE_LOG_FILE%/*}
-    liquibase status &>$HOME/${LIQUIBASE_TEMP_FILE}
-    grep "change sets have not been applied to" $HOME/${LIQUIBASE_TEMP_FILE} >/dev/null &&
-        info "Updating change sets to db..." &&
-        liquibase update &>$HOME/${LIQUIBASE_TEMP_FILE}
-    [[ $? == 0 ]] && info "Updated successfully!"
-    popd
+    liquibase status --defaults-file=$config_file &>$log_file
+    if [[ $? != 0 ]]; then
+        warn "Failed to check the database status. Check $schema_dir/$log_file"
+        return 1
+    fi
+    grep "change sets have not been applied to" $log_file >/dev/null
+    if [[ $? == 0 ]]; then
+        info "Updating change sets to db which defined by $config_file..."
+        liquibase update --defaults-file=$config_file &>$log_file
+        [[ $? == 0 ]] && info "Updated with $config_file successfully!" || warn "Failed to upgrade the database schema. Check $schema_dir/$log_file"
+    else
+        info "No change set was found to be executed, skiping..."
+    fi
+}
+
+function envToConfig() {
+    # which file should be rendered by envrionment variables
+    local config_file=$1
+    # get every envrionment variable's name
+    defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
+    # make template file for config file
+    cp $config_file $config_file.tmp
+    # render config file
+    info "Rendering $config_file using environment variables"
+    cat $config_file.tmp | envsubst "$defined_envs" >$config_file
+    # remove template file
+    rm $config_file.tmp
 }
